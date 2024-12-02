67fe295336c858014fecb0abbd665e0ac1132180
Noor Fatima <noorfatima262004@gmail.com>
2024-12-02 23:55:42 +0500

    SEO

--- Lab/pages/index.js
+++ Lab/pages/index.js
@@ -1,23 +1,37 @@
-// export default function Home() {
-//     return <h1>Hello, Static Site Generation!</h1>;
-//   }
 import { getPostData } from '../lib/posts';
+import Head from 'next/head';
 
-export default function Home({ postData }) {
+export default function Home({ data }) {
   return (
-    <div>
-      <h1>{postData.data.title}</h1>
-      <p>{postData.data.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: postData.content }} />
-    </div>
+    <>
+      {/* Adding SEO meta tags inside <Head> */}
+      <Head>
+      <title> My Static Site</title>
+      <meta name="description" content="This is a static site built with Next.js." />
+        <meta property="og:title" content={data.data.title} />
+        <meta property="og:description" content={data.content.substring(0, 160)} /> {/* Add a snippet of the content */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://noorfatima262004.github.io/static_site/" />
+        <meta property="og:image" content="\1.jpg" /> 
+        <meta name="robots" content="index, follow" />
+      </Head>
+
+      {/* The body content */}
+      <div>
+        <h1>{data.data.title}</h1>
+        <p>{data.data.date}</p>
+        <div dangerouslySetInnerHTML={{ __html: data.content }} />
+        <img src="\1.jpg" alt="Example Image" loading="lazy" />
+      </div>
+    </>
   );
 }
 
 export async function getStaticProps() {
-  const postData = getPostData();
+  const data = getPostData();
   return {
     props: {
-      postData,
+      data,
     },
   };
 }
